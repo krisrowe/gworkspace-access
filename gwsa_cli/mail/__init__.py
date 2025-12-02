c91a355a8d30c18f16fe09ad348b9c15d620c346
Kris Rowe <kris.rowe@rowelink.com>
2025-12-02 02:36:00 -0600

    Add drive.file scope for Google Drive file uploads
    Support file uploads to Google Drive (used by devws repo user-archive).

--- gwsa_cli/mail/__init__.py
+++ gwsa_cli/mail/__init__.py
@@ -10,7 +10,11 @@ from googleapiclient.errors import HttpError
 logger = logging.getLogger(__name__)
 
 # If modifying these scopes, the user_token.json file will need to be re-generated.
-SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
+# Run: gwsa setup --new-user
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/drive.file",  # Upload files to Drive (devws repo user-archive)
+]
 # Use ~/.config/gworkspace-access for storing credentials
 _CONFIG_DIR = os.path.expanduser("~/.config/gworkspace-access")
 USER_TOKEN_FILE = os.path.join(_CONFIG_DIR, "user_token.json")
